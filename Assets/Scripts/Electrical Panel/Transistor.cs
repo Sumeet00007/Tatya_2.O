65668996ae1245a3eae296b192b77c0a457c9b12
Mrunal Mendole <mbmendole@gmail.com>
2025-02-05 19:08:00 +0530

    Electrical Panel Fixed

--- Assets/Scripts/Electrical Panel/Transistor.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[DisallowMultipleComponent]
-public class Transistor : MonoBehaviour
-{
-    [Header("Transistor Properties")]
-    public bool isPlaced = false; // Tracks if the transistor has been placed.
-}
