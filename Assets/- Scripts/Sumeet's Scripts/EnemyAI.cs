fa30e38661aefd54b8a8de38e91c8ba70edd5160
Sumeeet-007 <sumeetwakde6@gmail.com>
2025-02-17 15:13:08 +0530

    EnemyAI-II
    Enemy chasing player done, also added jumpscare code if required.

--- Assets/- Scripts/Sumeet's Scripts/EnemyAI.cs
+++ Assets/- Scripts/Sumeet's Scripts/EnemyAI.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.SceneManagement;
 
 public class EnemyAI : MonoBehaviour
 {
     public NavMeshAgent agent;
     public List<Transform> destinations;
     public Animator anim;
-    public float walkSpeed, chaseSpeed, idleTime;
+    public float walkSpeed, chaseSpeed, idleTime, sightDistance,
+    catchDistance,chaseTime,minchaseTime,maxchaseTime,jumpScareTime;
     public int minIdleTime, maxIdleTime;
     public bool walking, chasing;
     public int destinationAmount;
@@ -17,17 +19,55 @@ public class EnemyAI : MonoBehaviour
     Transform currentDest;
     Vector3 dest;
     int randnum, randnum2;
+    public Vector3 raycastOffset;
+    //public string deathScene;
     
+ 
     void Start()
     {
         walking = true;
         randnum = Random.Range(0,destinationAmount);
         currentDest= destinations[randnum];
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        Vector3 direction=player.position-transform.position;
+        RaycastHit hit;
+        if(Physics.Raycast(transform.position+ raycastOffset,direction,out hit,sightDistance))
+        {
+            if(hit.collider.gameObject.tag=="Player")
+            {
+                walking=false;
+                StopCoroutine(StayIdle());
+                StopCoroutine(ChaseRoutine());
+                StartCoroutine(ChaseRoutine());
+                anim.ResetTrigger("walk");
+                anim.ResetTrigger("idle");
+                anim.SetTrigger("sprint");
+                chasing = true;
+            }
+        }
+
+        if(chasing ==true)
+        {
+            dest = player.position;
+            agent.destination = dest;
+            agent.speed = chaseSpeed;
+
+
+            if (agent.remainingDistance <= catchDistance)
+            {
+             // player.gameObject.SetActive(false);
+              anim.ResetTrigger("sprint");
+              //anim.SetTrigger("jumpScare");
+              //StartCoroutine(DeathRoutine());
+              chasing = false;
+            }
+        }
+
         if(walking==true)
         {
             dest = currentDest.position;
@@ -49,7 +89,7 @@ public class EnemyAI : MonoBehaviour
                     anim.SetTrigger("idle");
                     StopCoroutine(StayIdle());
                     StartCoroutine(StayIdle());
-
+                    walking = false;
                 }
             }
         }
@@ -65,6 +105,24 @@ public class EnemyAI : MonoBehaviour
         currentDest= destinations[randnum];
         anim.ResetTrigger("idle");
         anim.SetTrigger("walk");
+    }
+
+    IEnumerator ChaseRoutine()
+    {
+        chaseTime=Random.Range(minchaseTime,maxchaseTime);
+        yield return new WaitForSeconds(chaseTime);
+        walking = true;
+        chasing = false;
+        randnum=Random.Range(0,destinationAmount);
+        currentDest= destinations[randnum];
+        anim.ResetTrigger("sprint");
+        anim.SetTrigger("walk");
+    }
+
+    IEnumerator DeathRoutine()
+    {
+        yield return new WaitForSeconds(jumpScareTime);
+       // SceneManager.LoadScene(deathScene);
 
     }
 }
