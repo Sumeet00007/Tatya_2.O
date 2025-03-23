06b4deef1fb296d5f1fe20f5abdb41534e3eb84f
Sumeeet-007 <sumeetwakde6@gmail.com>
2025-03-23 15:06:58 +0530

    SpiderJumpScare Added

---
+++ Assets/- Scripts/Sumeet's Scripts/SpiderJumpScare.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SpiderJumpScare : MonoBehaviour
+{
+    public GameObject spiderPrefab;
+    public Transform spawnPoint;
+    private bool hasSpawned = false;
+
+    public Transform targetPosition;
+    public float speed = 7f;
+
+    private Vector3 initialPosition;
+
+    //reference for audioSource
+
+    void Start()
+    {
+        initialPosition = transform.position;
+    }
+
+
+
+    public void EnableSpiderJumpScare()
+    {
+        if (!hasSpawned && spiderPrefab != null)
+        {
+            //Play audioFX of Spider Crawling
+
+            GameObject spider = Instantiate(spiderPrefab, spawnPoint.position, spawnPoint.rotation);
+            StartCoroutine(MoveSpider(spider));
+            hasSpawned = true;
+        }
+    }
+
+
+    IEnumerator MoveSpider(GameObject spider)
+    {
+        while (spider != null && Vector3.Distance(spider.transform.position, targetPosition.position) > 0.1f)
+        {
+            spider.transform.position = Vector3.MoveTowards(spider.transform.position, targetPosition.position, speed * Time.deltaTime);
+            yield return null; 
+        }
+
+        if (spider != null)
+        {
+            Destroy(spider);
+        }
+    }
+  
+}
